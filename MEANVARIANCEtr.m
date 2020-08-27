7c1177c7a0c9d96b2b90992a927049abdd1c6a27
Liliwang1996 <55509442+Liliwang1996@users.noreply.github.com>
2020-08-27 14:13:32 +0800

    Add files via upload

---
+++ MEANVARIANCEtr.m
@@ -0,0 +1,40 @@
+all = csvread ('assets return.csv',3,1)
+TR=[1.024,	1.02,	1.022,	1.024,	1.016,	1.018,	1.02,	1.01,	1.024,	1.02,	1.022,	1.022,	1.02,	1.022,	1.024]
+xx = [];rr=[];stdv=[];var=[];
+for t = 1:15
+ for i = 1:60
+     for j = 1:40
+         monthly_R1(i,j) = all((t-1)*12+i,j)
+     end
+ end
+mu1 = exp(mean(log(monthly_R1)))'
+cov1 = cov(monthly_R1)
+
+% set range of values to use for return target R
+
+e = ones(40,1)
+ R = TR(t);
+ cvx_begin;
+  variable x(length(mu1))
+  minimize (x' * cov1 * x)
+  subject to
+    mu1'*x >= R;
+    e'*x == 1;
+    x>= 0;
+ cvx_end
+
+xx = [xx x]
+rr = [rr mu1'*x]
+var = [var x'*cov1*x]
+stdv = [stdv sqrt(x'*cov1*x)]
+end
+figure(1);
+plot(stdv, rr);
+title('Efficient Frontier')
+xlabel('Standard deviation')
+ylabel('Return')
+
+
+
+
+
