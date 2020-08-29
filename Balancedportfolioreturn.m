15e24e532ba492002f95668611ad61f97f7bdc96
Liliwang1996 <55509442+Liliwang1996@users.noreply.github.com>
2020-08-29 22:45:45 +0800

    Update Balancedportfolioreturn.m

--- Balancedportfolioreturn.m
+++ Balancedportfolioreturn.m
@@ -1,17 +1,16 @@
-% build a balanced portfolio
-all = csvread ('assets return.csv',3,1)
 rr=[]
+% build a balanced portfolio
 for m = 1:40
     s1(1,m) = 0.025
 end
 
-% Calculate the expected return of using a balanced portfolio from 2005 to 2019
+% Calculate the realized return of using a balanced portfolio from 2005 to 2019
 for t = 1:15
  for i = 1:12
      for j = 1:40
-         monthly_R1(i,j) = all(60+i+12*(t-1),j)
+         monthly_R1(i,j) = all(60+i+12*(t-1),j)% get each yeas' monthly return
      end
  end
-mu1 = exp(mean(log(monthly_R1)))'
+mu1 = exp(mean(log(monthly_R1)))' % Geometric mean of monthly return of 40 assets per year
 rr = [rr mu1'*s1']
 end
